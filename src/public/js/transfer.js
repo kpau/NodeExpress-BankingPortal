cef8428e450628699d614aba9034643a0d8c2b69
Kristiyan Nikolov <kristiyan.nikolov@bet365.com>
2019-08-19 14:18:00 +0300

    eslint and editorconfig

--- src/public/js/transfer.js
+++ src/public/js/transfer.js
@@ -1,17 +1,17 @@
-let from = document.querySelector('#from');
-let to = document.querySelector('#to');
-let transferForm = document.querySelector('#transferForm');
+const from = document.querySelector('#from');
+const to = document.querySelector('#to');
+const transferForm = document.querySelector('#transferForm');
 
-function validate(evt){
+function validate(evt) {
   evt.preventDefault();
   document.querySelector('.error').innerHTML = '';
-  if(from.value === to.value) {
+  if (from.value === to.value) {
     document.querySelector('.error').innerHTML = 'You can not transfer to the same account!';
-  } else if (evt.type === "submit") {
+  } else if (evt.type === 'submit') {
     transferForm.submit();
   }
 }
 
-to.addEventListener("change", validate);
-from.addEventListener("change", validate);
-transferForm.addEventListener('submit', validate)
+to.addEventListener('change', validate);
+from.addEventListener('change', validate);
+transferForm.addEventListener('submit', validate);
