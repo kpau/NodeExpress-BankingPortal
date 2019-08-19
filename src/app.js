eecb19149a92ad18338f779da3b78805e7a428de
Kristiyan Nikolov <kristiyan.nikolov@bet365.com>
2019-08-19 15:17:54 +0300

    Module 2: File Handling and Routing

--- src/app.js
+++ src/app.js
@@ -11,9 +11,40 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+const accountData = fs.readFileSync(path.join(__dirname, 'json/accounts.json'), { encoding: 'UTF8' });
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync(path.join(__dirname, 'json/users.json'), { encoding: 'UTF8' });
+const users = JSON.parse(userData);
+
 app.get('/', (req, res) => {
   res.render('index', {
-    title: 'Index',
+    title: 'Account Summary',
+    accounts,
+  });
+});
+
+app.get('/savings', (req, res) => {
+  res.render('account', {
+    account: accounts.savings,
+  });
+});
+
+app.get('/checking', (req, res) => {
+  res.render('account', {
+    account: accounts.checking,
+  });
+});
+
+app.get('/credit', (req, res) => {
+  res.render('account', {
+    account: accounts.credit,
+  });
+});
+
+app.get('/profile', (req, res) => {
+  res.render('profile', {
+    user: users[0],
   });
 });
 
